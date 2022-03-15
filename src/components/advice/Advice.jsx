bdd9d95d19df83424da62164145133e3a949ad08
Nico Blasutig <nicolasblasutig@gmail.com>
2022-03-14 21:01:10 -0300

    mobile view

--- src/components/advice/Advice.jsx
+++ src/components/advice/Advice.jsx
@@ -12,6 +12,9 @@ const initialQuote = {
 
 }
 
+const svgDesktop = <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+const svgMobile = <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+
 const Advice = () => {
 
   const [quote, setQuote] = useState(initialQuote)
@@ -20,7 +23,6 @@ const Advice = () => {
     getQuote()
       .then((newQuote) =>{
         setQuote(newQuote)
-
       })
   }
 
@@ -43,7 +45,7 @@ const Advice = () => {
         </div>
 
         <div className='advice__box-line'>
-          <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
+          {svgDesktop}
         </div>
 
         <div className='advice__button'>
--- src/helpers/getQuote.js
+++ src/helpers/getQuote.js
@@ -1,8 +1,6 @@
 const getQuote = async () =>{
-    let userId = Math.floor(Math.random()*190)
-    console.log(userId)
+    let userId = Math.floor(Math.random()*180+1)
     const url = "https://api.adviceslip.com/advice/"+userId
-    console.log(url)
     const response =  await fetch(url)
     const quote = await response.json()
 
